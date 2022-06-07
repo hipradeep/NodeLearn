c36022ec85a055c8ddc2ca45dc8ab719fe3a0382
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-07 11:18:35 +0530

    updt

---
+++ day2/classfun.js
@@ -0,0 +1,12 @@
+
+
+module.exports = function(){
+    
+
+    this.p="pradeep";
+    this.m="maurya";
+
+    this.print = ()=> {
+        console.log("My name is "+ this.p+" "+this.m)
+    }
+}
--- day2/index.js
+++ day2/index.js
@@ -1,4 +1,8 @@
 const {p, pp} = require("./cal");
+var  classfun = require('./classfun');
 
 console.log("HI");
 console.log(pp());
+
+var s=new classfun();
+s.print();
