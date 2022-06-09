f01c6c08eaf03c1b28799dde59713b964e443b9d
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-09 12:12:00 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //import express from 'express';
-const express= require('express')
+const express = require('express')
 var app = express();
 // import path from 'path';
 // const __dirname = path.resolve(path.dirname('')); 
@@ -8,29 +8,24 @@ var app = express();
 //    res.send('Hello World qqqq');
 // })
 
-app.get('/',(req,res)=>
-{
-    console.log(__dirname);
+app.get('/', (req, res) => {
+  console.log(__dirname);
   //  res.sendFile(path.join(__dirname ,'/','form.html'))
-    res.sendFile(__dirname+"/"+"form.html")
-   
- //res.send("root page"+ __dirname)
-//console.log("inside root function")
+  res.sendFile(__dirname + "/" + "form.html")
+
+  //res.send("root page"+ __dirname)
+  //console.log("inside root function")
 })
 app.get('/admin', function (req, res) {
-    res.send('Hello World Admin');
- })
- app.get('/reg',(req,res)=>
- {
- res.send( "welcome "+req.query.user+ "  "+req.query.pass )
- 
- }
- )
+  res.send('Hello World Admin');
+})
+app.get('/regpage', (req, res) => {
+  res.send("welcome " + req.query.user + "  " + req.query.pass)
+})
 
 
 var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    
-    console.log("Server runnng on http://localhost:8081",host, port );
- })
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Server runnng on http://localhost:8081", host, port);
+})
