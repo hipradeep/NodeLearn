4cb9b16c78e79b9d2daa5104a5dae5d9e2cfd462
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-10 23:07:47 +0530

    upd

--- _httpModules/index2.js
+++ _httpModules/index2.js
@@ -5,8 +5,20 @@ import http from 'http';
 const server=http.createServer((req, res)=>{
 
     res.setHeader('Content_Type', 'text/plain');
+    
     //res.end('Response From Server prdp');
-    res.end(req.url) // send end url
+
+    //res.end(req.url) // send end url
+
+    //we can also handle the response accordind to url
+    if(req.url=='/') res.end("Home");
+    else if(req.url=='/about') res.end("About");
+    else if(req.url=='/pradeep') res.end("Pradeep");
+    else if(req.url=='/contact-us') res.end("Contact Us");
+    else if(req.url=='/resume') res.end("Resume");
+    else if(req.url=='/profile') res.end("Profile");
+    else res.end('Wrong End Url');
+
 })
 
 const PORT=8080;
