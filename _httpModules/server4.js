450cab9dfcfdd5f35feb6c0883419f476ea7ebba
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-25 02:22:10 +0530

    http

--- _httpModules/server4.js
+++ _httpModules/server4.js
@@ -10,15 +10,44 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
 
     //send html file
-    fs.readFile('./views/index.html', (err, data) => {
-      if (err) {
-        console.log(err);
-        res.end();
-      }
-      //res.write(data);
-      res.end(data);
-    });
+    // fs.readFile('./views/index.html', (err, data) => {
+    //   if (err) {
+    //     console.log(err);
+    //     res.end();
+    //   }
+    //   //res.write(data);
+    //   res.end(data);
+    // });
 
+      // routing
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about'); //redirect to about
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+  }
+   // send html
+   fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    //res.write(data);
+    res.end(data);
+  });
 
 });
 // localhost is the default value for 2nd argument
