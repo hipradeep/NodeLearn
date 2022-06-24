3f0d5943eaacc860bd72b3f0c19ecb9b9dc4657e
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-25 02:30:10 +0530

    lodash

---
+++ _impPackages/lodash.js
@@ -0,0 +1,12 @@
+const _ =require('lodash')
+
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('hello');
+  });
+  greet();
+  greet();
+  greet();
