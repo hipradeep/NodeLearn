f664b414604385b37054b46034510324433e6e5c
hipradeep <pradeepmaurya.cse@gmail.com>
2022-06-25 02:08:15 +0530

    http

---
+++ _httpModules/server4.js
@@ -0,0 +1,27 @@
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    console.log(req.url);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    //send html file
+    fs.readFile('./views/index.html', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      }
+      //res.write(data);
+      res.end(data);
+    });
+
+
+});
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+});
